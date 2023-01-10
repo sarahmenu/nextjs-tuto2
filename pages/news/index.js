989dcb27d25bfd2f279903cc76de5de519dd7c82
Sarah Menu <sarah.menu@gmail.com>
2023-01-10 15:02:51 +0100

    added nested pages and links

---
+++ pages/news/index.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+import Link from "next/link";
+
+function Newspage() {
+  return (
+    <Fragment>
+      <h1> The News Page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-great-framework'>NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
+}
+
+export default Newspage
